d6f9d5155abf74c8713ef00ca86463b73037accc
Umar Sheikh <sheikhgarts@gmail.com>
2022-12-08 23:28:08 +0500

    Metamask

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -26,7 +26,7 @@ const MetaMaskContainer = styled(Box)(({theme }) => ({
     cursor: "pointer",
     width: "100%",
     height: "100%",
-    background: "red",
+    background: "yellow",
 }))
 function Login({
   open,
